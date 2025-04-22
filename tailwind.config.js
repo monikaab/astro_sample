52693401ec8d25cf40404772340d6c4d0246bd45
monikaab <monikaab26@gmail.com>
2025-04-22 01:02:12 -0500

    Adding themer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-module.exports = {
-  // ...
-  safelist: [
-    'bg-mint-100',
-    'bg-mint-200',
-    'bg-mint-300',
-    'bg-mint-400',
-    'bg-mint-500',
-    'bg-mint-600',
-    'bg-mint-700',
-    'bg-mint-800',
-    'bg-mint-900'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./pages/**/*.{astro,html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
@@ -24,6 +18,47 @@ module.exports = {
           700: '#2C7A7B',
           800: '#285E61',
           900: '#234E52'
+        },
+        purple: {
+          80: "#F3F4F6"
+        }
+      }
+    }
+  },
+  plugins: [],
+};
+
+
+module.exports = {
+  // ...
+  safelist: [
+    'bg-mint-100',
+    'bg-mint-200',
+    'bg-mint-300',
+    'bg-mint-400',
+    'bg-mint-500',
+    'bg-mint-600',
+    'bg-mint-700',
+    'bg-mint-800',
+    'bg-mint-900',
+    'bg-purple-80'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mint: {
+          100: 'oklch(0.71 0.15 247)',
+          200: 'oklch(0.69 0.10 186)',
+          300: 'oklch(0.76 0.16 71)',
+          400: 'oklch(0.69 0.17 293)',
+          500: 'oklch(0.543 0.028 283)',
+          600: '#319795',
+          700: '#2C7A7B',
+          800: '#285E61',
+          900: '#234E52'
+        },
+        purple: {
+          80: "#F3F4F6"
         }
       }
     }
