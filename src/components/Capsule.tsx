ae94f27141bf0edc3971f78b9ba2f7fb62586175
monikaab <monikaab26@gmail.com>
2025-04-26 08:58:34 -0500

    Refactor Capsule component styles for consistency; update Tailwind configuration to add new color shades and improve color organization

--- src/components/Capsule.tsx
+++ src/components/Capsule.tsx
@@ -10,8 +10,8 @@ type CapsuleProps = {
 const Capsule: Component<CapsuleProps> = (props) => {
   return (
     <div
-      class={`flex items-center justify-center w-fit min-w-11 h-6 gap-1.5 px-2.5 py-0 rounded-lg font-inter font-bold text-xs leading-[1.5] tracking-normal
-${props.textColor ?? "text-white"} text-xs ${props.color ?? "bg-orange-400"}`}
+      class={`flex items-center justify-center w-fit min-w-11 h-6 gap-1.5 px-2.5 py-0 rounded-lg font-inter font-semiBold text-s leading-[1.5] tracking-normal
+${props.textColor ?? "text-white"} ${props.color ?? "bg-orange-400"}`}
 
     >
       <span>{props.text}</span>
